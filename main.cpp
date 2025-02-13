d8e8288b62e436e20bacb1dcf7ff0c188257f31b
FreemanJournal <iamishaque.business@gmail.com>
2025-02-14 00:16:05 +0600

    1. Fizz Buzz Problem Solution

--- main.cpp
+++ main.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 int main() {
-
+    int low = 1, high = 3;
+    int inCount = (high - low) / 2;
+    if (low % 2 != 0 || high % 2 != 0) {
+        inCount++;
+    }
+    cout << inCount << endl;
     return 0;
 }
 
